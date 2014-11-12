c9f69a82a1e7249ccaf2fbdfc9c5e8cc07c87d9e
s.bernardinello <s.bernardinello@reply.it>
2014-11-12 12:38:36 +0100

    Started Sqoop Command

--- src/beans/MyBean.java
+++ src/beans/MyBean.java
@@ -1,5 +1,7 @@
 package beans;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -7,7 +9,8 @@ import javax.inject.Named;
 
 @Named
 @RequestScoped
-public class MyBean {
+public class MyBean implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private String myText;
 
 	public void addMessage() {
---
+++ src/sqoop/SqoopCommand.java
@@ -0,0 +1,71 @@
+package sqoop;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class SqoopCommand implements Serializable {
+	private static final long serialVersionUID = 1L;
+	protected String importCommand = "sqoop import";
+	protected String username="username", password="pass", 
+			connectionUrl = "jdbc:db://hostname:port";
+	protected String finalCommand;
+	
+	public void generate() {
+		finalCommand = 
+				importCommand +
+				" --username "+ username +
+				" --password "+ password +
+				" --connect " + connectionUrl
+		;
+				
+	}
+	
+	public void clear() {
+//		for( Field f : this.getClass().getDeclaredFields() ) {
+//			if( String.class.isAssignableFrom(f.getType()) ) {
+//				try {
+//					f.set(this, "");
+//				} catch (IllegalArgumentException | IllegalAccessException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+		this.finalCommand = "";
+	}
+	
+	public String getFinalCommand() {
+		return finalCommand;
+	}
+	public void setFinalCommand(String finalCommand) {
+		this.finalCommand = finalCommand;
+	}
+	public String getImportCommand() {
+		return importCommand;
+	}
+	public void setImportCommand(String importCommand) {
+		this.importCommand = importCommand;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConnectionUrl() {
+		return connectionUrl;
+	}
+	public void setConnectionUrl(String connectionUrl) {
+		this.connectionUrl = connectionUrl;
+	}
+
+}
