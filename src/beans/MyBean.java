13d7db7c155a16667a69bc312248ba4952a03309
s.bernardinello <s.bernardinello@reply.it>
2014-11-12 10:36:21 +0100

    Growl trial

--- src/beans/MyBean.java
+++ src/beans/MyBean.java
@@ -1,6 +1,8 @@
 package beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
@@ -8,6 +10,11 @@ import javax.inject.Named;
 public class MyBean {
 	private String myText;
 
+	public void addMessage() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		fc.addMessage(null, new FacesMessage("Your text: ", myText));
+	}
+	
 	public String getMyText() {
 		return myText;
 	}
